ab174dbc4b13fe7caed631bd3c61132f50cd059e
Yana <142252119+MorozovaYana@users.noreply.github.com>
2024-10-15 12:57:40 +0300

    Update main.cpp

--- Documents/git/lb3/main.cpp
+++ Documents/git/lb3/main.cpp
@@ -1,39 +1,39 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    
-//    int N;
-//    int r = 0;
-//    
-//    cout << "Enter the value N: ";
-//    cin >> N;
-//    
-//    if (N >= 100 && N <= 999) {
-//        
-//        int x = N / 100;
-//        int y = (N / 10) % 10;
-//        int z = N % 10;
-//        
-//        if(x >= 5 ) {
-//            r += x;
-//        }
-//        
-//        if(y >= 5 ) {
-//            r += y;
-//        }
-//        
-//        if(z >= 5 ) {
-//            r += z;
-//        }
-//        
-//        cout << "Resuts: " << r << "\n";
-//    }
-//    else{
-//        cout << "Error"<< "\n";
-//    
-//    }
-//       
-//    
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   int N;
+   int r = 0;
+   
+   cout << "Enter the value N: ";
+   cin >> N;
+   
+   if (N >= 100 && N <= 999) {
+       
+       int x = N / 100;
+       int y = (N / 10) % 10;
+       int z = N % 10;
+       
+       if(x >= 5 ) {
+           r += x;
+       }
+       
+       if(y >= 5 ) {
+           r += y;
+       }
+       
+       if(z >= 5 ) {
+           r += z;
+       }
+       
+       cout << "Resuts: " << r << "\n";
+   }
+   else{
+       cout << "Error"<< "\n";
+   
+   }
+      
+   
+   return 0;
+}
