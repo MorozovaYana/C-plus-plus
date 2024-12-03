6575bfdd09fa0e65c61a60a345de6c0a0d57a18d
Yana <142252119+MorozovaYana@users.noreply.github.com>
2024-12-03 13:32:54 +0200

    Update main.cpp

--- Documents/git/lr9/main.cpp
+++ Documents/git/lr9/main.cpp
@@ -1,50 +1,50 @@
-//#include <iostream>
-//#include <cstdlib>
-//#include <ctime>
-//using namespace std;
-//
-//int main() {
-//    const int size = 10;
-//    int arr[size];
-//
-//    
-//   srand(time_t(NULL));
-//
-//    for (int i = 0; i < size; i++) {
-//        arr[i] = rand() % 100 + 1;
-//    }
-//
-//
-//    cout << "The Arrays with random numbers: ";
-//    for (int i = 0; i < size; i++) {
-//        cout << arr[i] << " ";
-//    }
-//    cout << endl;
-//
-//   
-//    int minIndex = 0, maxIndex = 0;
-//
-//
-//    for (int i = 1; i < size; i++) {
-//        if (arr[i] < arr[minIndex]) {
-//            minIndex = i;
-//        }
-//        if (arr[i] > arr[maxIndex]) {
-//            maxIndex = i;
-//        }
-//    }
-//
-//
-//    int temp = arr[minIndex];
-//    arr[minIndex] = arr[maxIndex];
-//    arr[maxIndex] = temp;
-//
-//    
-//    cout << "Arrays after changes: ";
-//    for (int i = 0; i < size; i++) {
-//        cout << arr[i] << " ";
-//    }
-//    cout << endl;
-//
-//    return 0;
-//}
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+   const int size = 10;
+   int arr[size];
+
+   
+  srand(time_t(NULL));
+
+   for (int i = 0; i < size; i++) {
+       arr[i] = rand() % 100 + 1;
+   }
+
+
+   cout << "The Arrays with random numbers: ";
+   for (int i = 0; i < size; i++) {
+       cout << arr[i] << " ";
+   }
+   cout << endl;
+
+  
+   int minIndex = 0, maxIndex = 0;
+
+
+   for (int i = 1; i < size; i++) {
+       if (arr[i] < arr[minIndex]) {
+           minIndex = i;
+       }
+       if (arr[i] > arr[maxIndex]) {
+           maxIndex = i;
+       }
+   }
+
+
+   int temp = arr[minIndex];
+   arr[minIndex] = arr[maxIndex];
+   arr[maxIndex] = temp;
+
+   
+   cout << "Arrays after changes: ";
+   for (int i = 0; i < size; i++) {
+       cout << arr[i] << " ";
+   }
+   cout << endl;
+
+   return 0;
+}
