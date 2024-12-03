9a00d52b423dc587ad9bd8affddbc1d967bc2ffb
Yana <142252119+MorozovaYana@users.noreply.github.com>
2024-12-03 13:32:19 +0200

    Update main.cpp

--- Documents/git/lr7/main.cpp
+++ Documents/git/lr7/main.cpp
@@ -1,37 +1,37 @@
-//
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    
-//    int c0;
-//    int count = 0;
-//    
-//    cout << "Enter c0 ";
-//    cin >> c0;
-//    
-//    while (c0 > 0) {
-//        if (c0 % 2 == 0) {
-//            c0 /= 2;
-//        }else {
-//            c0 = c0*3 +1;
-//        }
-//        
-//        if ( c0 > 1 ) {
-//            cout << c0 << "\n";
-//            count += 1;
-//            continue;
-//        }else{
-//            count += 1;
-//            cout << c0 << "\n";
-//            cout << "steps = "<< count << "\n";
-//            break;
-//            
-//        }
-//        
-//    }
-//    
-// 
-//    
-//    return 0;
-//}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   int c0;
+   int count = 0;
+   
+   cout << "Enter c0 ";
+   cin >> c0;
+   
+   while (c0 > 0) {
+       if (c0 % 2 == 0) {
+           c0 /= 2;
+       }else {
+           c0 = c0*3 +1;
+       }
+       
+       if ( c0 > 1 ) {
+           cout << c0 << "\n";
+           count += 1;
+           continue;
+       }else{
+           count += 1;
+           cout << c0 << "\n";
+           cout << "steps = "<< count << "\n";
+           break;
+           
+       }
+       
+   }
+   
+
+   
+   return 0;
+}
