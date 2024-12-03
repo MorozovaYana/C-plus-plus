fc317dd24640c255af20ef74b1a332c753c97c47
Yana <142252119+MorozovaYana@users.noreply.github.com>
2024-12-03 13:32:32 +0200

    Update task2.cpp

--- Documents/git/lr7/task2.cpp
+++ Documents/git/lr7/task2.cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//#include <iomanip>
-//using namespace std;
-//
-//int main(void) {
-//    
-//    double pi4 = 0.;
-//    long n;
-//    cout << "Number of iterations? ";
-//    cin >> n;
-//
-//    for (long k = 0; k < n; ++k) {
-//            if (k % 2 == 0) {
-//                pi4 += 1.0 / (2.0 * k + 1.0);
-//            } else {
-//                pi4 -= 1.0 / (2.0 * k + 1.0); 
-//            }
-//        }
-//    
-//    cout << fixed << setprecision(20);
-//    cout << "Pi = " << (pi4 * 4.) << endl;
-//    
-//return 0;
-//}
-//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(void) {
+   
+   double pi4 = 0.;
+   long n;
+   cout << "Number of iterations? ";
+   cin >> n;
+
+   for (long k = 0; k < n; ++k) {
+           if (k % 2 == 0) {
+               pi4 += 1.0 / (2.0 * k + 1.0);
+           } else {
+               pi4 -= 1.0 / (2.0 * k + 1.0); 
+           }
+       }
+   
+   cout << fixed << setprecision(20);
+   cout << "Pi = " << (pi4 * 4.) << endl;
+   
+return 0;
+}
+
